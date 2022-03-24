ead2ad57e0de5b323fb734bf1708c30d36b8c92f
Santi Bouhid <santifbouhid@gmail.com>
2022-03-24 18:39:40 -0300

    un poco de limpieza en avatares.js

--- avatar.js
+++ avatar.js
@@ -8,10 +8,8 @@ const btnCambiarAvatarUsuario3 = document.getElementById ('btnCambiarAvatarUsuar
 const avatarUsuario4 = document.getElementById('avatarUsuario4')
 const btnCambiarAvatarUsuario4 = document.getElementById ('btnCambiarAvatarUsuario4');
 
-const btnCerrarModal = document.getElementById('btnCerrarModal');
-const modalAvatares = document.getElementById('modalAvatares');
-
-
+// const btnCerrarModal = document.getElementById('btnCerrarModal');
+// const modalAvatares = document.getElementById('modalAvatares');
 
 let nuevoAvatar1 = document.createElement ('div');
 let nuevoAvatar2 = document.createElement ('div');
@@ -19,7 +17,7 @@ let nuevoAvatar3 = document.createElement ('div');
 let nuevoAvatar4 = document.createElement ('div');
 
 
-const avatarItem = document.getElementsByClassName ('avatar');
+// const avatarItem = document.getElementsByClassName ('avatar');
 
 const URL_AVATARS = "json/avatares.json";
 
@@ -28,71 +26,13 @@ const btnAceptarCambioAvatar = document.getElementById ('btnAceptarCambioAvatar'
 
 let arrayAvarates = [] ;
 
-let a = document.getElementsByClassName('contenedorAvatar');
-let b = document.getElementsByClassName('avatar');
+// let a = document.getElementsByClassName('contenedorAvatar');
+// let b = document.getElementsByClassName('avatar');
 
-console.log(a);
+// console.log(a);
 
-
-const insertarAvatares = () => {
-  fetch(URL_AVATARS)
-  .then(resultado => resultado.json())
-  .then(respuesta => {
-    for (let avatar of respuesta){
-      arrayAvarates.push (avatar);
-    }
-    console.log(respuesta);
-    arrayAvarates = respuesta;
-    // console.log(arrayAvarates);
-
-    // console.log(typeof(arrayAvarates));
-    console.log(typeof(arrayAvarates));
-    console.log(arrayAvarates[0]);
-    pintarAvatares();
-  })
-}
-
-
-// for (let avatar of arrayAvaratrees){
-//   const element = document.createElement ('button');
-//   element.classList.add('contenedorAvatar');
-//   element.innerHTML = 
-//   `<img  class="avatar" src=${avatar.url}>`;
-//   contenedorListaAvatares.appendChild(element);
-// }
-
-
-insertarAvatares();
-
-console.log(arrayAvarates[1]);
-
-
-
-
-function pintarAvatares () {
-  for (let avatar of arrayAvarates){
-    const element = document.createElement ('button');
-    element.classList.add('contenedorAvatar');
-    element.innerHTML = 
-    `<img  class="avatar" src=${avatar.url}>`;
-    contenedorListaAvatares.appendChild(element);
-    seleccionarAvatar(element, avatar);
-  }
-  
-}
-
-
-const borrarListaAvatares = () => {
-  // let a = document.getElementsByClassName('contenedorAvatar');
-  // console.log(a);
-  for (let i = a.length - 1; i >= 0; --i) {
-    a[i].remove();
-  }         
-}
-
-
+//* FUNCION PARA CALIFICAR AL AVATAR COMO "SELECCIONADO"
 const seleccionarAvatar = (element, av) => {
-   
   element.onclick = () => {
     for (let avatar of arrayAvarates){
       avatar.seleccionado=false;
@@ -104,29 +44,37 @@ const seleccionarAvatar = (element, av) => {
 }
 
 
-// btnAceptarCambioAvatar.onclick = (nuevoAvatar, avatarUsuario, btnCambiarAvatarUsuario) => {
-//   for (let avatar of arrayAvarates){
-//     if (avatar.usuario == numeroUsuario){
-//       avatar.usuario = 0;
-//     }
-//   }
-//   if (avatar.seleccionado==true) {
-//     nuevoAvatar.remove();
-//     avatarUsuario.remove();
-//     console.log(avatar);
-//     //nuevoAvatar = document.createElement ('div');
-//     nuevoAvatar.classList.add('avatarPokemon');
-//     nuevoAvatar.innerHTML = 
-//     `<img id="avatarUsuario" class="avatarPokemon" src=${avatar.url}>`;
-//     btnCambiarAvatarUsuario.appendChild(nuevoAvatar);
-//     avatar.usuario=numeroUsuario;
-//   }
+//* FUNCION PARA COLOCAR LOS AVATARES EN EL MODAL.
+function pintarAvatares () {
+  for (let avatar of arrayAvarates){
+    const element = document.createElement ('button');
+    element.classList.add('contenedorAvatar');
+    element.innerHTML = 
+    `<img  class="avatar" src=${avatar.url}>`;
+    contenedorListaAvatares.appendChild(element);
+    seleccionarAvatar(element, avatar);
+  }
+}
 
+//* CREO UN ARRAY LOCAL Y COLOCO LOS AVATARES EN EL MODAL.
+const insertarAvatares = () => {
+  fetch(URL_AVATARS)
+  .then(resultado => resultado.json())
+  .then(respuesta => {
+    for (let avatar of respuesta){
+      arrayAvarates.push (avatar);
+    }
+    console.log(respuesta);
+    //arrayAvarates = respuesta;
+    // console.log(arrayAvarates);
+    // console.log(typeof(arrayAvarates));
+    console.log(typeof(arrayAvarates));
+    console.log(arrayAvarates[0]);
+    pintarAvatares();
+  })
+}
 
-// }
-
-//* modificar el avatar en el card
-
+//* MODIFICA EL AVATAR EN EL CARD.
 const modificarAvatar = (nuevoAvatar, avatarUsuario, btnCambiarAvatarUsuario, numeroUsuario) => {
   btnAceptarCambioAvatar.onclick = () => {
     for (let avatar of arrayAvarates){
@@ -152,8 +100,6 @@ const modificarAvatar = (nuevoAvatar, avatarUsuario, btnCambiarAvatarUsuario, nu
 }
 
 
-
-
 const insertarAvatares1 = () => {
   modificarAvatar(nuevoAvatar1, avatarUsuario1, btnCambiarAvatarUsuario1, 1);
 }
@@ -168,76 +114,13 @@ const insertarAvatares4 = () => {
 }
 
 
-
-
 btnCambiarAvatarUsuario1.addEventListener('click', insertarAvatares1);
 btnCambiarAvatarUsuario2.addEventListener('click', insertarAvatares2);
 btnCambiarAvatarUsuario3.addEventListener('click', insertarAvatares3);
 btnCambiarAvatarUsuario4.addEventListener('click', insertarAvatares4);
 
-// btnCerrarModal.addEventListener('click', borrarListaAvatares);
 
+//* CODIGO.
+insertarAvatares();
 
-
-
-
-
-
-// const insertarAvatares = (nuevoAvatar,avatarUsuario, btnCambiarAvatarUsuario, numeroUsuario) => {
-//   fetch(URL_AVATARS)
-//   .then(resultado => resultado.json())
-//   .then(respuesta => {
-    
-//     console.log(respuesta);
-//     arrayAvarates = respuesta;
-//     // console.log(arrayAvarates);
-
-//     // console.log(typeof(arrayAvarates));
-//     console.log(typeof(arrayAvarates));
-
-
-//     for (let avatar of arrayAvarates){
-//       const element = document.createElement ('button');
-//       element.classList.add('contenedorAvatar');
-//       element.innerHTML = 
-//       `<img  class="avatar" src=${avatar.url}>`;
-//       contenedorListaAvatares.appendChild(element);
-    
-
-    
-//       element.onclick = () => {
-//         for (let avatar of arrayAvarates){
-//           avatar.seleccionado=false;
-//         }
-//         avatar.seleccionado=true;
-//         console.log(avatar);
-//         console.log(arrayAvarates);
-//         console.log(avatar.usuario);
-//         btnAceptarCambioAvatar.onclick = () => {
-//           for (let avatar of arrayAvarates){
-//             if (avatar.usuario == numeroUsuario){
-//               avatar.usuario = 0;
-//             }
-//           }
-//           if (avatar.seleccionado==true) {
-//             nuevoAvatar.remove();
-//             avatarUsuario.remove();
-//             console.log(avatar);
-//             //nuevoAvatar = document.createElement ('div');
-//             nuevoAvatar.classList.add('avatarPokemon');
-//             nuevoAvatar.innerHTML = 
-//             `<img id="avatarUsuario" class="avatarPokemon" src=${avatar.url}>`;
-//             btnCambiarAvatarUsuario.appendChild(nuevoAvatar);
-//             avatar.usuario=numeroUsuario;
-            
-//           }
-//           borrarListaAvatares();
-//         }
-
-//       }
-      
-//     }
-    
-//   }) 
-  
-// }
+// console.log(arrayAvarates[1]);
